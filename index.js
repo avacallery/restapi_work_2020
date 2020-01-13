34b0d3aabebfe964df4101000c2501f0d60b5ae3
avacallery <avacallery@gmail.com>
2020-01-13 13:43:47 -0500

    CRUD Commands with MONGODB

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose'); 
 
-mongoose.connect('mongodb+srv://ava_rest_api:avatest20@cluster0-9qzld.mongodb.net/test?retryWrites=true&w=majority', 
+mongoose.connect('mongodb+srv://USERNAME:PASSWORD@cluster0-9qzld.mongodb.net/test?retryWrites=true&w=majority', 
 { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('Connected to MongoDB...'))
     .catch(err => console.error('Could not connect to MongoDB...', err))
