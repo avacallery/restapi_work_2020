43b890657079d86505d2115e47e3638270a7658a
avacallery <avacallery@gmail.com>
2020-01-14 13:53:05 -0500

    Chapter 8 Customers in Database

--- vidly/index04.js
+++ vidly/index04.js
@@ -1,16 +1,18 @@
 const mongoose = require('mongoose'); 
 const genres = require('./routes/genres'); 
+const customers = require('./routes/customers')
 const express = require('express'); 
 const app = express(); 
 
 
-mongoose.connect('mongodb+srv://avacallery:<PASSWORD>@crudcluster-ksmuy.mongodb.net/test?retryWrites=true&w=majority',
+mongoose.connect('mongodb+srv://avacallery:avatest20@crudcluster-ksmuy.mongodb.net/test?retryWrites=true&w=majority',
     { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('Connected to MongoDB...'))
     .catch(err => console.error('Could not connect to MongoDB...', err))
 
     app.use(express.json()); 
     app.use('/api/genres', genres); 
+    app.use('/api/customers', customers); 
 
     const port = process.env.PORT || 3000; 
     app.listen(port, () => console.log (`Listening on port ${port}...`));
