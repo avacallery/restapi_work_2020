edd230bf7025367f4044933319ec09e1b039412f
avacallery <avacallery@gmail.com>
2020-01-18 19:17:19 -0500

    embedding

--- embedding.js
+++ embedding.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/playground')
+mongoose.connect('mongodb://localhost/playground', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB...'))
   .catch(err => console.error('Could not connect to MongoDB...', err));
 
@@ -15,34 +15,53 @@ const Author = mongoose.model('Author', authorSchema);
 //embed an author document directly inside the course document 
 const Course = mongoose.model('Course', new mongoose.Schema({
   name: String,
-  author: {
-    type: authorSchema, 
-    required: true
-  }
+  authors: [authorSchema]
   }));
 
-async function createCourse(name, author) {
+
+async function addAuthor(courseId, author) {
+  const course = await Course.findById(courseId); 
+  course.authors.push(author); 
+  course.save();
+}
+
+addAuthor('5e2242eb370fbb387d09f2b1', )
+
+async function createCourse(name, authors) {
   const course = new Course({
     name, 
-    author
-  }); 
+    authors
+  });  
   
   const result = await course.save();
   console.log(result);
 }
+
 
 async function listCourses() { 
   const courses = await Course.find();
   console.log(courses);
 }
 
+
 async function updateAuthor(courseId) {
   const course = await Course.update( { _id: courseId }, {
     $unset: {
-      'author': ''
+      'author': '',
     }
   });
 }
 
-//createCourse('Node Course', new Author({ name: 'Mosh' }));
-updateAuthor('5e223c0874bff73784d6cb4f'); 
+async function removeAuthor(courseId, authorId) {
+  const course = await Course.findById(courseId); 
+  const author = course.authors.id(authorId); 
+  author.remove(); 
+  course.save(); 
+}
+
+// createCourse('Node Course', [
+//     new Author({ name: 'Mosh' }), 
+//     new Author({ name: 'John' })
+// ]);
+
+// removeAuthor('5e2242eb370fbb387d09f2b1', '5e224591ed6b24389d33462f' )
