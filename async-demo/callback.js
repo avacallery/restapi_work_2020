d11cbf5609290c2e3c887c0274d33b9807209ede
avacallery <avacallery@gmail.com>
2020-01-27 12:24:34 -0500

    callback hell

--- async-demo/callback.js
+++ async-demo/callback.js
@@ -9,29 +9,43 @@
 //PROMISES aND ASYNC AWAIT WILL SAVE YOU FROM CALLBACK HELL
 
 
-console.log('Before'); 
-getUser(1, (user) => {
-getRepositories(user.gitHubUsername, (repos) => {
-    console.log('Repos', repos); 
-}); 
-})
-console.log('After'); 
+console.log('Before');
+getUser(1, userRepos);
+console.log('After');
+
+function userRepos(user) {
+    getRepos(user.gitHubUsername, getCommits)
+}
+
+function getCommits(repos) {
+    console.log('Repos', repos)
+    getCommits(repo, displayCommits); 
+}
+
+function displayCommits(commits) {
+    console.log(commits); 
+}
+
 
 function getUser(id, callback) {
     setTimeout(() => {
         console.log('Reading a user from a database...');
-        callback({ id: id, gitHubUsername: 'mosh' });  
-    }, 2000); 
+        callback({ id: id, gitHubUsername: 'mosh' });
+    }, 2000);
 }
 
-function getRepositories(username, callback) {
+function getRepos(username, callback) {
     setTimeout(() => {
-        console.log('Calling GitHub API...'); 
+        console.log('Calling GitHub API...');
         callback(['repo1', 'repo2', 'repo3'])
     }, 2000);
 }
 
-
+// function getCommits(repo, callback) {
+//     setTimeout(() => {
+//         console.log('Getting commits...');
+//     }, 2000);
+// }
 // console.log('Before'); 
 // getUser(1, getRespositories); 
 // console.log('After'); 
